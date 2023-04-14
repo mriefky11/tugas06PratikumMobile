ceed89a807b07adf6184d94d21286ee7f79f3c58
rendial26 <2006030@itg.ac.id>
2023-04-15 00:34:32 +0700

    data berita

---
+++ lib/data/news_data.dart
@@ -0,0 +1,125 @@
+import '../models/news.dart';
+
+List<News> newsList = [
+  News(
+    id: 1,
+    title:
+        "Megawati Kumpulkan Anggota Fraksi PDIP Seusai Penolakan Timnas Israel",
+    image:
+        "https://static.republika.co.id/uploads/images/inpicture_slide/megawati_230116162936-290.jpg?w=650&q=90",
+    content: '''
+      REPUBLIKA.CO.ID, JAKARTA -- Wakil Ketua Komisi VI DPR Fraksi Partai Demokrasi Indonesia Perjuangan (PDIP) membenarkan bahwa Megawati Soekarnoputri mengumpulkan seluruh anggota Fraksi PDIP pada Sabtu (8/4/2023) lalu. Salah satu pembahasannya adalah penguatan kepada kader partai seusai penolakan terhadap kehadiran tim nasional Israel di Indonesia. "Yang jelas ada kontraksi yang akibat penolakan Israel U-20 itu, kan banyak yang lembaga survei mengatakan elektabilitas PDI Perjuangan kan turun. Nah, ini dikuatkan. Semakin kita diberikan tantangan PDI Perjuangan ini kan semakin kuat," ujar Aria Bima di Gedung Nusantara II, Kompleks Parlemen, Jakarta, Kamis (13/4).
+    ''',
+  ),
+  News(
+    id: 2,
+    title: "Pengamat Nilai Jokowi dan Megawati Penentu Capres Koalisi Besar",
+    image:
+        "https://static.republika.co.id/uploads/images/inpicture_slide/perayaan-hut-ke-50-pdip-dihadiri-langsung-oleh-presiden-joko_230110092933-983.jpg",
+    content: '''
+      REPUBLIKA.CO.ID, JAKARTA -- Direktur Eksekutif PARA Syndicate Ari Nurcahyo menilai Presiden Joko Widodo (Jokowi) dan Ketua Umum PDI Perjuangan Megawati Soekarnoputri merupakan sosok penentu calon presiden (capres) 2024 yang akan diusung koalisi besar.
+
+"Tapi orkestrasinya itu bergantung dua tokoh sebagai kingmaker. Pertama adalah Pak Jokowi dan kedua adalah Ibu Megawati," ujar Ari dalam diskusi bertajuk "Koalisi Besar untuk Siapa: Ganjar atau Prabowo atau ", di Jakarta, Jumat.Koalisi besar mewacanakan penggabungan Koalisi Indonesia Bersatu (KIB), Koalisi Kebangkitan Indonesia Raya (KKIR) dan PDI Perjuangan. KIB terdiri dari Partai Golkar, PAN, dan PPP. KKIR beranggotakan Partai Gerindra dan PKB. KIB dan KKIR membuka diri apabila PDI Perjuangan bergabung dalam koalisi besar. Ari menilai wacana pembentukan koalisi besar sejauh ini masih dalam tahap penjajakan terutama soal siapa sosok capres-cawapres yang akan diusung nantinya. Ia juga tidak menampik sosok yang bakal diusung oleh koalisi besar merupakan keputusan dari para ketua umum partai politik (parpol).
+
+"Ini untuk siapa koalisi besar ini? Yang jelas koalisi besar tidak bisa dilepaskan memang kuasanya ada di ketum-ketum partai politik dari enam partai politik," katanya.
+
+Dia menilai Jokowi dan Megawati akan turut menimbang dan menentukan siapa sosok yang layak untuk maju sebagai capres dari koalisi besar. Ari melihat nama Prabowo dan Ganjar berada pada daftar teratas sebagai kandidat capres.
+
+"Pak Jokowi ingin melampirkan I am The King Maker, tapi Pak Jokowi tidak ingin melepaskan ikatan politiknya dengan Bu Mega dan tetap ingin mengorkestrasi partai-partai politik yang ada sambil menimbang siapa ini Prabowo atau Ganjar, Ganjar atau Prabowo, Prabowo-Ganjar atau GanjarPrabowo," katanya.
+
+
+    ''',
+  ),
+  News(
+    id: 3,
+    title:
+        "Pengamat: Jika Puan tak Jadi Presiden atau Wapres, Peralihan dari Megawati tak Mulus",
+    image:
+        "https://static.republika.co.id/uploads/images/inpicture_slide/ketua-dpp-pdip-puan-maharani-buka-suara-terkait-wacana_230318144611-414.jpg",
+    content: '''
+      REPUBLIKA.CO.ID, BENGKULU  -- Pakar politik sekaligus akademisi Universitas Bengkulu Panji Suminar menilai periode Pemilu 2024 menjadi 'masa-masa emasnya' karier politik Ganjar Pranowo, Anies Baswedan dan Puan Maharani. Terkhusus Puan, ini menjadi penting karena ia digadang akan menjadi pengganti Megawati."Kalau mengejar politik untuk menjadi presiden atau wakil presiden, Pemilu 2024 ini lah masa-masa emas mereka," kata Panji Suminar di Bengkulu, Jumat.
+    ''',
+  ),
+  News(
+    id: 4,
+    title:
+        "Gubernur I Wayan Koster Kini Sambut Israel ke Bali dengan Tangan Terbuka",
+    image:
+        "https://static.republika.co.id/uploads/images/inpicture_slide/065945500-1653306298-830-556.jpg",
+    content: '''
+      REPUBLIKA.CO.ID, JAKARTA -- Gubernur Bali I Wayan Koster tak lagi berkeras menentang Israel. Ia kini menyambut dengan tangan terbuka kedatangan atlet-atlet Israel ke daerahnya untuk mengikuti ANOC World Beach Games 2023.  Koster menyatakan dukungannya terhadap pelaksanaan ANOC World Beach Games 2023 saat bertemu dengan Ketua KOI Raja Sapta Oktohari dan Menpora Dito Ariotedjo. Koster berharap penyelenggaraan bisa berjalan dengan lancar dan sesuai konstitusi. “Baru saja kami berdiskusi bersama Pak Menpora dan Pak Okto (Ketum KOI). Kita membicarakan agar bagaimana World Beach Games ini berjalan dengan baik dan segi tata kelola berjalan dengan lancar,” kata Koster, dikutip dari laman resmi Kemenpora. “Oleh karena itu, sudah ada kesepakatan Bali tuan rumah, maka Pemprov Bali dan saya tentunya, akan memberi dukungan penuh agar acara ini berjalan lancar dan sukses. Intinya World Beach Games sesuai amanat konstitusi,” jelas Koster. Menpora Dito memastikan penyelenggaraan ANOC World Beach Games 2023 di Bali tetap berjalan sesuai agenda. Hal ini dipastikan usai diskusi di Rumah Dinas Gubernur Bali, Denpasar, Jumat (14/4). Menpora Dito mengatakan, Gubernur Koster sangat mendukung penuh pelaksanaan ajang multi-olahraga internasional itu.  “Hasil diskusi membawa keberkahan, ada satu pertemuan dan kesepakatan bahwa Pemerintah Provinsi Bali dan Gubernur Bali seribu persen mendukung penyelenggaraan World Beach Games 2023,” kata Menpora Dito.  Menpora Dito memang sebelumnya terus berupaya untuk mendorong komunikasi yang intens agar pelaksanaan World Beach Games 2023 bisa dilaksanakan di Bali. Hingga akhirnya Menpora Dito memediasi antara Gubernur Koster dengan Ketua Komite Olimpiade Indonesia (KOI) Raja Sapta Oktohari secara langsung.  “Alhamdulillah pada suasana Ramadhan ini kita menjalin silaturahmi dan bisa mencapai hasil-hasil yang jadi satu titik temu yaitu kesepakatan tentang ini,” ujar Menpora Dito. 
+    ''',
+  ),
+  News(
+    id: 5,
+    title:
+        "Pemuda Asal Jawa Timur Ini Siap Guncang MotoGP di Texas Amerika Serikat, Mulai dari Moto3",
+    image:
+        "https://static.republika.co.id/uploads/images/inpicture_slide/023308900-1647780203-830-556.jpg",
+    content: '''
+      REPUBLIKA.CO.ID, AUSTIN -- Pembalap Indonesia Mario Suryo Aji memiliki ekspektasi tinggi untuk meraih poin balapan Moto3 di Austin, Texas, Amerika Serikat pada akhir pekan ini. Pemuda kelahiran Magetan, Jawa Timur, 16 Maret 2004 ini menjalani tahun keduanya berseragam Honda Team Asia di kelas paling ringan dari kejuaraan dunia MotoGP tersebut dan akan berusaha menegaskan kemajuan pada balapan ketiga musim 2023 tersebut."Saya tiba di Circuit of the Americas dalam kondisi yang jauh lebih baik dari tahun lalu," kata Mario dikutip laman resmi tim, Jumat (14/4/2023).Secara fisik saya bekerja keras, dan soal pengendaraan, saya merasa juga lebih baik," kata pebalap 19 tahun itu. Mario belum meraih poin pada dua balapan pembuka menyusul hasil peringkat 18 di Portugal dan tidak menyelesaikan lomba di Argentina.Pada debutnya tahun lalu, Mario finis di peringkat 26 di klasemen dengan koleksi lima poin, salah satunya ia raih dengan finis di urutan ke-14 di Mandalika, Lombok. "Di Argentina, saya kehilangan peluang besar mencetak poin. Untuk meraihnya di Austin akan rumit, tapi tim dan saya akan melakukan yang terbaik untuk meraih tujuan ini," kata Mario. Seperti diungkapkan Manajer tim Hiroshi Aoyama, Sirkuit Austin(COTA) dengan panjang lintasan 5,513km merupakan sirkuit yang cukup besar dibandingkan trek-trek lain di kalender. "Ini sangat menantang bagi para pebalap dan mekanik ketika mereka akan menyetel motornya," kata Aoyama. "Mario dan Taiyo (Furusato, rekan satu tim) menunjukkan progres, saya ingin melihat apa yang mereka bisa lakukan di sirkuit ini. "Saya ingin melihat hasil yang mereka raih. Semuanya positif dan tiba di Austin dengan semangat yang baik." Setelah dua balapan, pembalap tim Red Bull KTM Tech3 Daniel Holgado asal Spanyol memimpin klasemen dengan 38 poin, diikuti pebalap MSI Diogo Moreira asal Brazil dengan 36 pin dan Tatsuki Suzuki asal Jepang dari tim Leopard Racing dengan 27 poin.
+    ''',
+  ),
+];
+
+List<News> hotNewsList = [
+  News(
+    id: 1,
+    title:
+        "Mourinho Enggan Saksikan Derbi Italia di Liga Champions, Pilih Tonton Madrid Vs Chelsea",
+    image:
+        "https://static.republika.co.id/uploads/images/inpicture_slide/036840100-1676181595-830-556.jpg",
+    content: '''
+REPUBLIKA.CO.ID, JAKARTA -- Manajer Roma, José Mourinho, mengatakan bahwa ia tidak menonton derbi Serie A di Liga Champions antara Milan dan Napoli demi mendukung Real Madrid-Chelsea, dua mantan klubnya.Ketika ditanya apakah ia akan menghabiskan malam dengan menonton pertandingan dua tim Serie A yang akan berlaga di San Siro pada leg pertama perempat final Liga Champions, Mourinho menjawab: "Tidak, saya tidak akan melakukannya, saya akan menonton Real Madrid-Chelsea."
+
+Giallorossi akan bertemu Feyenoord pada leg pertama perempat final Europa League malam ini, hampir satu tahun setelah kemenangan mereka atas tim asal Belanda tersebut di Final Conference League.Mourinho yakin Roma bisa menjuarai Liga Eropa musim ini, namun kedalaman skuatnya masih menjadi perhatian: "Jika tim seperti Inter dan Milan kesulitan untuk bermain dua kali dalam sepekan, bayangkan kami," lanjutnya.
+
+"Liga ini sulit, tim-tim di paruh kedua klasemen memiliki banyak kualitas." jelasnya.
+
+Milan akhirnya mengalahkan Napoli 1-0 di San Siro, sementara Real Madrid asuhan Carlo Ancelotti meraih kemenangan 2-0 atas Chelsea di Estadio Bernabeu. Sementara Roma justru keok, mengakui keunggulan Feyenoord 0-1 di tanah Belanda dalam leg pertama Liga Europa.
+
+
+''',
+  ),
+  News(
+    id: 2,
+    title: "Diperiksa Penyidik, Kekasih Dandy, AGH Masih Berstatus Saksi",
+    image:
+        "https://static.republika.co.id/uploads/images/inpicture_slide/030980100-1677213414-830-556.jpg",
+    content: '''
+REPUBLIKA.CO.ID, JAKARTA -- Penyidik Polres Jakarta Selatan telah melakukan pemeriksaan terhadap kekasih Mario Dandy Satrio berinisial AGH (15 tahun) atas kasus penganiayaan terhadap anak salah satu Pengurus Pusat GP Ansor, Crytalino David Ozora (17 tahun). Namun sampai saat ini AGH yang juga mantan kekasih dari korban masih berstatus sebagai saksi. "Jam 22.00 WIB (Sabtu malam) sebenernya selesainya. Sudah selesai pemeriksaan, statusnya masih saksi anak," ujar Kuasa hukum A, Mangatta Toding Allo, kepada awak media Ahad (26/2). Dalam kesempatan itu, Mangatta juga mengatakan, bahwa pihaknya telah melaporkan kepada Komisi Perlindungan Anak Indonesia (KPAI) untuk menjaga kliennya. Menurutnya, kliennya tersebut tidak memiliki niatan terlibat dalam penganiayaan terhadap korban David. Karena itu, diharapkan nama baik AGH dipulihkan kembali.
+
+"Kami juga laporkan ke KPAI untuk adanya tindakan-tindakan menjaga saksi kami ini klien kami ini, agar nama baiknya dipulihkan kembali," harap Mangatta.Selain itu, kata Mangatta, kliennya membantah telah melakukan pose foto selfie terhadap David setelah  dianiaya oleh Mario Dandy. Disebutnya apa yang dituduhkan di media sosial terkait keterlibatan AGH dalam kasus penganiayaan tersebut tidaklah benar. Apalagi yang bersangkutan masih  anak di bawah umur. 
+
+"Pak Kapolres sudah menyampaikan keterangan yang disampaikan saksi APA ini bahwa klien kami ini, Agnes tidak melakukan hal-hal yang tidak dinginkan seperti yang dituduhkan di medsos," jelas Mangatta.
+
+Dalam kasus ini, penyidik Polres Metro Jakarta Selatan telah menetapkan dua tersangka yaitu Mario dan temannya berinisial SRLPL (19 tahun). Mario dijerat dengan Pasal 76c juncto Pasal 80 Undang-Undang Nomor 35 Tahun 2014 tentang perubahan atas Undang-Undang RI Nomor 23 Tahun 2002 tentang Perlindungan Anak subsider Pasal 351 ayat 2 tentang penganiayaan berat. 
+
+Sementara itu, SRLPL disangkakan dengan Pasal 76 huruf C juncto Pasal 80 Undang-Undang Nomor 35 Tahun 2014 tentang Perubahan atas Undang-Undang Nomor 23 Tahun 2002 tentang Perlindungan Anak. Diduga SRLPL berperan sebagai provokator hingga merekam adegan penganiayaan yang dilakukan Mario terhadap David. 
+
+Peristiwa penganiayaan itu terjadi di kawasan Pesanggrahan, Jakarta Selatan, Senin (20/2) lalu. Awalnya AGH diduga sebagai sosok yang pertama yang mengadu kepada Mario jika mendapat perlakuan kurang baik dari korban.
+
+Namun, belakangan diketahui orang yang pertama memberikan informasi jika orang yang pertama kali memberikan informasi kepada Mario mengenai kabar kekasihnya, AGH diperlakukan tak baik yakni temannya berinisial APA.
+
+Pada tanggal 17 Januari 2023 APA melaporkan kepada Mario bahwa saksi AGH mendapat perlakuan tak baik dari David. Mendengar kabar itu, lalu Mario mengkonfirmasi langsung kepada AGH.
+
+Lalu AGH pun membenarkan jika dirinya mendapat perlakuan tidak baik oleh korban. Sehingga melalui AGH Mario dapat bertemu dengan korban dan melakukan penganiayaan keji. 
+
+"Setelah dibenarkan (oleh AGH) itulah yang membuat tersangka MDS emosi dan mengajak anak korban untuk bertemu," terang Kapolres Metro Jakarta Selatan, Kombes Ade Ary Syam Indradi.
+
+''',
+  ),
+  News(
+    id: 3,
+    title: "Infografis Rafael Alun Ditahan KPK",
+    image:
+        "http://static.republika.co.id/uploads/infografis/rafael-alun-akhirnya-ditahan_230404175131-404.jpg",
+    content: '''
+REPUBLIKA.CO.ID, KPK akhirnya menahan eks pejabat Ditjen Pajak Kemenkeu, Rafael Alun Trisambodo pada Senin (3/4/2023). Rafael ditahan seusai pemeriksaannya sebagai tersangka kasus dugaan penerimaan gratifikasi.
+JERATAN PASAL: Pasal 12B Undang-Undang Pemberantasan Tindak Pidana Korupsi.ANCAMAN PIDANA: Penjara seumur hidup atau penjara paling singkat 4 tahun dan paling lama 20 tahun dan pidana denda paling sedikit Rp 200 juta dan paling banyak Rp 1 miliar.
+BARANG BUKTI DISITA: 30 tas mewah berbagai merek, seperti Hermes, Christian Dior, hingga Louis Vuitton; dompet; ikat pinggang; jam tangan; Uang senilai Rp 3 2,2 miliar dalam pecahan mata uang asing yang ditemukan dalam //safe deposit box// di salah satu bank.
+GRATIFIKASI: KPK juga menemukan adanya aliran  gratifikasi yang diterima Rafael melalui perusahaannya, yakni PT Artha Mega Ekadhana (AME). Dari perusahaan yang bergerak di bidang jasa konsultansi pembukuan dan perpajakan ini, Rafael diduga memperoleh 90 ribu dolar AS.
+"Untuk kepentingan penyidikan, RAT dilakukan penahanan selama 20 hari pertama," kata Ketua KPK Firli Bahuri di Gedung Merah Putih KPK, Jakarta Selatan, Senin (3/4/2023).
+        ''',
+  ),
+];
